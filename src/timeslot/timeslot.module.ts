2951d78ae502e53712a7ecde42f63d182ee9678a
Lucas Nouguier <lucas.nouguier@protonmail.com>
2023-01-31 12:54:28 +0100

    feat: timeslots crud

---
+++ src/timeslot/timeslot.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TimeslotService } from './timeslot.service';
+import { TimeslotController } from './timeslot.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Timeslot } from './entities/timeslot.entity';
+
+@Module({
+  imports: [SequelizeModule.forFeature([Timeslot])],
+  controllers: [TimeslotController],
+  providers: [TimeslotService],
+})
+export class TimeslotModule {}
