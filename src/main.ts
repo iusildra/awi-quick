22fdbc8b9c937e31b7f35e31859d7c0ee07964bb
Lucas Nouguier <lucas.nouguier@protonmail.com>
2023-03-06 04:39:44 +0100

    fix: used npx instead of global

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port =
+    process.env.PORT == null || process.env.PORT == ''
+      ? 3000
+      : process.env.PORT;
+
   const app = await NestFactory.create(AppModule);
   app.enableCors({});
 
@@ -14,6 +19,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
